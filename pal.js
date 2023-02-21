fa29262d8abf8d77ee877c2d631981e4c64031b4
Sourav Dhua <souravdhua1997@gmail.com>
2023-02-21 22:18:42 +0530

    done it

--- pal.js
+++ pal.js
@@ -1,1 +1,25 @@
 Check whether a number is Prime or not
+
+let num=17;
+if(isPrime(num)){
+console.log('Prime Number');
+}else{
+console.log('Not Prime');
+}
+function isPrime(){
+	if(num==0 || num==1){
+		return false;
+	}else{
+		let cnt=0;
+		for(let i=2;i<num**(1/2);i++){
+			if(num%i==0){
+				cnt++;
+			}
+		}
+		if(cnt==0){
+			return true;		
+		}else{
+			return false;
+		}
+	}  	
+}
